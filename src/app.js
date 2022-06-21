f24ec74c3c7b0fa4f7325c2a61f2b09ed09b6498
nizarmonza <nizarmonzaa@gmail.com>
2022-06-21 23:20:16 +0000

    random-card-generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#topSuit").innerHTML = indexSuits;
+  document.querySelector("#bottomSuit").innerHTML = indexSuits;
+  document.querySelector("#number").innerHTML = indexNumbers;
 };
+
+let numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let lastNumber = numbers.length - 1;
+let indexNumbers = numbers[Math.floor(Math.random() * lastNumber)];
+
+let suits = ["♦", "♥", "♠", "♣"];
+let lastSuit = suits.length - 1;
+let indexSuits = suits[Math.floor(Math.random() * lastSuit)];
+
+let btn = document.createElement("button");
+btn.innerHTML = "New Card";
+btn.addEventListener("click", function() {
+  window.location.reload();
+});
+document.body.appendChild(btn);
